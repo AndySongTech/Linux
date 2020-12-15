08b444fd4cac8c723da1a2e8a991886dd23b9005
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-15 22:24:45 +0800

    Update 003. create bulk users.sh

--- shell/003. create bulk users.sh
+++ shell/003. create bulk users.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-for i in 
+for i in {1..20}
+do
+    adduser user$i
+    pass=`echo $RANDOM | md5sum | cut -c 1-8`
+    echo $pass | passwd --stdin user$i
+    echo -e "User_Name: user$i \n Password: $pass" >> account.txt
+
+done
+
+
+
+
+# delete bulk users
+
+#!/bin/bash
+for i in {1..20}
+do
+    adddel -r user$i   # remove user home dir
+   
+
+done
