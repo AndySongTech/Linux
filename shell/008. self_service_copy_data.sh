308d9eab83a85b3c1def0cd8ae355d5deb51f0ce
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-18 15:02:27 +0800

    Update 008. self_service_copy_data.sh

--- shell/008. self_service_copy_data.sh
+++ shell/008. self_service_copy_data.sh
@@ -3,16 +3,21 @@ read -p "Please input the file that you want to copy: " file
 if [ -f $file -o -d $file ]; then
   read -p "Are you confirm to copy $file?(y/n)" sure
   confirm=$(echo ${sure} | tr A-Z a-z)
-  if [ "$confirm" == "y" ]: then
+  if [ "$confirm" == "y" ]; then
     read -p "Please input the destination directory: " dest
     dest_dir=$(echo ${dest})
     if [ -d $dest_dir ]; then
       cp $file $dest_dir
       echo "The $file has been moved to $dest_dir!"
-    else 
+    else
       echo "The destination is not exist"
-  else 
-    echo "Okay"
+      exit 1
+    fi
+  elif [ "$confirm" == "n" ]; then
+    echo "Bye"
+  else
+    echo "Please input y/n: "
+  fi
 else
-
+  echo "The is not exsit"
 fi
