5278648fa28ecafcff0afcfd2c11b9f5487c9027
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-18 15:18:22 +0800

    Update 009. calculator.sh

--- shell/009. calculator.sh
+++ shell/009. calculator.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-read -p " "
+read -p " Input the first number: " num_fst
+read -p " Input the operator: "  operator
+read -p " Input the second number: " num_sec
+echo " $num_fst$operator$num_sec=$(($num_fst$operator$num_sec))"
