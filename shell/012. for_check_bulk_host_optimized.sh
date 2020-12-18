41381a1a1837f84e320fdba7dbdfe77e7df0b3c6
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-18 16:22:00 +0800

    Update 012. for_check_bulk_host_optimized.sh

--- shell/012. for_check_bulk_host_optimized.sh
+++ shell/012. for_check_bulk_host_optimized.sh
@@ -2,7 +2,7 @@
 . /etc/init.d/functions
 ip=172.16.101.
 for i in {1..100}
-  do
+do
   if ping -c 1 -w 1 $ip$i &>/dev/null; then
     echo -n  "$ip$i"
     success
@@ -13,4 +13,4 @@ for i in {1..100}
     echo ""
   fi
 
-  done
+done
