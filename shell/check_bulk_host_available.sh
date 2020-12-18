0a1295427bdfdb8982bba9c60bd63994296ee8b7
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-18 11:08:17 +0800

    Update and rename check_bulk_host_available to check_bulk_host_available.sh

--- shell/check_bulk_host_available.sh
+++ shell/check_bulk_host_available.sh
@@ -1,7 +1,7 @@
 ip=172.16.101.
 for i in {1..100}
   do 
-  if ping -c 1 $ip$1 &>/dev/null; then
+  if ping -c 1 $ip$i &>/dev/null; then
     echo "$ip$i is running"
   else 
     echo "$ip$i is not running"
