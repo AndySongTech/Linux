6ee3fe35882fa5bf07eab140d38c04c51edeeea1
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-20 15:10:53 +0800

    Create guess_number.sh

---
+++ shell/guess_number.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+num=`echo "$RANDOM" | cut -c 1-2`
+while true
+do
+  read -p "Input the number in range 0-99: " num1
+  if [ $num1 -lt $num ]; then
+    echo "The number is greater than your input, try it again. "
+    continue
+  elif [ $num1 -gt $num ]; then
+    echo " The number is less than your input, try it again. "
+    continue
+  elif [ $num1 -eq $num ]; then
+    echo " You got it. Awesome!!! "
+    break
+  else
+    echo " See you next time. "
+    exit
+
+  fi
+done
