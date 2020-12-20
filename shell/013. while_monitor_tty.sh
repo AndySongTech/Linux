135c5f73de0b68c61ef648542d774fc6967c76ac
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-20 13:11:44 +0800

    Update 013. while_monitor_tty.sh

--- shell/013. while_monitor_tty.sh
+++ shell/013. while_monitor_tty.sh
@@ -8,6 +8,11 @@ do
   
 done
 
-# bash while_monitor_tty.sh &  # 在后台执行
-# nohup bash while_monitor_tty.sh &  # 在后台执行,及时terminal关掉，依然会秩序写入tty.txt
+# 运行脚本：
+# bash while_monitor_tty.sh  # 在current终端运行，退出或关闭终端脚本及停止运行
+# bash while_monitor_tty.sh & # 在后台运行， 关闭终端后脚本停止运行
+# nohup bash while_monitor_tty.sh &  # 在后台运行，不挂起，在服务器运行的情况下可以一直运行,会秩序写入到tty.txt，及时终端关闭或退出。
 # tail -f /root/shell/tty.txt   # 秩序显示tty.txt里的更新内容
+# 终止脚本运行：
+# ps -aux | grep while_monitor_tty.sh  # 找到相应的进程PID
+# kill -9 15470  # kill 对应的进程既可以终止脚本运行
