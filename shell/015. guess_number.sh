f49cf3aacdd0e8b6c88e130a4788d1b58add7555
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2020-12-20 15:50:10 +0800

    Update 015. guess_number.sh

--- shell/015. guess_number.sh
+++ shell/015. guess_number.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 num=`echo "$RANDOM" | cut -c 1-2`
 while true
 do
@@ -6,9 +7,15 @@ do
   if [ $num1 -lt $num ]; then
     echo "The number is greater than your input, try it again. "
     continue
+    
+  elif [ $num1 -ge 100 ]; then
+    echo " The number is exceed the rang. "
+    continue
+    
   elif [ $num1 -gt $num ]; then
     echo " The number is less than your input, try it again. "
     continue
+  
   elif [ $num1 -eq $num ]; then
     echo " You got it. Awesome!!! "
     break
