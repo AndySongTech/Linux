847caaf3472fdc6d9e620a9e0e5dd0238292b8ab
Andy Song <50960053+AndySongTech@users.noreply.github.com>
2021-03-17 23:22:01 +0800

    Create 022. backup_mysql.sh

---
+++ shell/022. backup_mysql.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+# (1) set up all the mysqldump variables
+FILE=database.sql.`date +"%Y%m%d"`
+DBSERVER=myserver.com
+DATABASE=mydatabase
+USER=myuser
+PASS=mypass
+
+# (2) in case you run this more than once a day, remove the previous version of the file
+unalias rm     2> /dev/null
+rm ${FILE}     2> /dev/null
+rm ${FILE}.gz  2> /dev/null
+
+# (3) do the mysql database backup (dump)
+
+# use this command for a database server on a separate host:
+mysqldump --opt --protocol=TCP --user=${USER} --password=${PASS} --host=${DBSERVER} ${DATABASE} > ${FILE}
+
+# use this command for a database server on localhost. add other options if need be.
+#mysqldump --opt --user=${USER} --password=${PASS} ${DATABASE} > ${FILE}
+
+# (4) gzip the mysql database dump file
+gzip $FILE
+
+# (5) show the user the result
+echo "${FILE}.gz was created:"
+ls -l ${FILE}.gz
